3e1600d2d31eb7f51661957e719aa0e24e155681
Cássio Zen <cassiozen@gmail.com>
2018-01-02 22:38:44 -0500

    path fix

---
+++ view.js
@@ -0,0 +1,15 @@
+module.exports = (body) => {
+  return (`<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${body}
+    </div>
+  </body>
+  </html>`)
+}
