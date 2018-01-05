8ef1c8d2c777d748f45b42d8b0804c6d5e40d5ee
Cássio Zen <cassiozen@gmail.com>
2018-01-05 15:56:27 -0500

    upvotes

--- app.js
+++ app.js
@@ -6,29 +6,26 @@ const postDetails = require("./views/postDetails");
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
 
-const baseQuery = "SELECT posts.*, users.name AS author FROM posts INNER JOIN users ON users.id = posts.user_id\n";
-
+const baseQuery = "SELECT posts.*, users.name AS author, counting.upvotes FROM posts INNER JOIN users ON users.id = posts.user_id INNER JOIN (SELECT post_id, COUNT(*) as upvotes FROM upvotes GROUP BY post_id) AS counting ON posts.id = counting.post_id\n";
 
 app.get("/", async (req, res) => {
   try {
     const data = await client.query(baseQuery);
-    res.send(postList(data.rows));  
+    res.send(postList(data.rows));
   } catch (error) {
-    
     res.status(500).send(`Something went wrong: ${error}`);
   }
 });
 
 app.get("/posts/:id", async (req, res) => {
   try {
-    const data = await client.query(baseQuery + "WHERE posts.id = $1", [req.params.id])
+    const data = await client.query(baseQuery + "WHERE posts.id = $1", [req.params.id]);
     const post = data.rows[0];
-    res.send(postDetails(post));  
+    res.send(postDetails(post));
   } catch (error) {
-    console.error(error)
     res.status(500).send(`Something went wrong: ${error}`);
   }
 });
