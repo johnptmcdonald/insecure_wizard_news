944d6f0401df4636b745330ec37f5f0fcfd71860
Cássio Zen <cassiozen@gmail.com>
2018-01-15 15:29:55 -0500

    Part 3

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
 const express = require("express");
 const morgan = require("morgan");
-const client = require("./db");
-const postList = require("./views/postList");
-const postDetails = require("./views/postDetails");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}))
 
-const baseQuery = "SELECT posts.*, users.name AS author, counting.upvotes FROM posts INNER JOIN users ON users.id = posts.user_id INNER JOIN (SELECT post_id, COUNT(*) as upvotes FROM upvotes GROUP BY post_id) AS counting ON posts.id = counting.post_id\n";
+app.use("/posts", require("./routes/posts"));
 
-app.get("/", async (req, res) => {
-  try {
-    const data = await client.query(baseQuery);
-    res.send(postList(data.rows));
-  } catch (error) {
-    res.status(500).send(`Something went wrong: ${error}`);
-  }
-});
+app.get("/", (req, res) => {
+  res.redirect("/posts");
+})
 
-app.get("/posts/:id", async (req, res) => {
-  try {
-    const data = await client.query(baseQuery + "WHERE posts.id = $1", [req.params.id]);
-    const post = data.rows[0];
-    res.send(postDetails(post));
-  } catch (error) {
-    res.status(500).send(`Something went wrong: ${error}`);
-  }
-});
 
 const PORT = 1337;
 
--- views/postDetails.js
+++ views/postDetails.js
@@ -12,14 +12,9 @@ module.exports = (post) => html`<!DOCTYPE html>
     <header><img src="/logo.png"/>Wizard News</header>
       <div class='news-item'>
       <p>
-        <span class="news-position">${post.id}. ▲</span> ${post.title} <small>(by ${
-          post.author
-        })</small>
+        ${post.title} <small>(by ${post.name})</small>
       </p>
-      <small class="news-info">
-        ${post.upvotes} upvotes | ${timeAgo(post.date)}
-      </small>
-      <p class="news-info">${post.body}</p>
+      <p>${post.content}</p>
     </div>
   </div>
 </body>
